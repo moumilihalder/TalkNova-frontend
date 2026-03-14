432320abc8b34947c897a28a826e655fedf28d15
Moumili Halder <moumilihalder@gmail.com>
2026-03-14 13:24:14 +0530

    change the vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // important for routing on Vercel
+  build: {
+    outDir: 'dist', // Vercel will serve this folder
+  },
   server: {
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api": "http://localhost:5000", // works locally
     },
   },
 });
-
-
